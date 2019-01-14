9c9fe3942cbf0f4f62b32422902890b7b5a6a6d1
William Dunk <williamdunk@gmail.com>
2019-01-14 14:25:47 +0000

    v1.2 added initial balance of zero

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,4 +1,10 @@
 class Oystercard
-  def balance
+
+  attr_reader :balance 
+
+  def initialize(amount = 0)
+    @balance = amount
   end
+
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,9 @@ describe Oystercard do
   it "has a balance" do
     expect(subject).to respond_to(:balance)
   end
+
+  it "has initial balance of 0" do
+    expect(subject.balance).to eq(0)
+  end
+
 end
