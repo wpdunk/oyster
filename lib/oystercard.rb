82d587bed1c41ad03681ba9d9da1416b2faf1dd4
William Dunk <williamdunk@gmail.com>
2019-01-15 14:27:33 +0000

    added MIN_BALANCE constant to prevent touch in

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,10 +3,12 @@ class Oystercard
   attr_reader :balance
 
   MAX_BALANCE = 90
+  MIN_BALANCE = 1
 
   def initialize
     @balance = 0
     @travelling = false
+
     # @journey = false
   end
 
@@ -28,6 +30,7 @@ class Oystercard
   end
 
   def touch_in
+    fail "balance too low" if @balance < MIN_BALANCE
     @travelling = true
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -25,42 +25,42 @@ describe Oystercard do
       expect { subject.topup 1 }.to raise_error "max amount reached"
     end
   end
-describe "#deduct" do
-it "can be deducted" do
-  subject.topup(10)
-  expect(subject.deduct(2)).to eq(8)
-  end
-end
 
-describe "#journey?" do
-
-  it 'is initially not in a journey' do
-    expect(subject).not_to be_travelling
+  describe "#deduct" do
+  it "can be deducted" do
+    subject.topup(10)
+    expect(subject.deduct(2)).to eq(8)
+    end
   end
 
-  it "can touch in" do
-    subject.touch_in
-    expect(subject).to be_travelling
+  describe "#journey?" do
+
+    it 'is initially not in a journey' do
+      expect(subject).not_to be_travelling
+    end
+
+    it "can touch in" do
+      subject.topup(Oystercard::MIN_BALANCE)
+      subject.touch_in
+      expect(subject).to be_travelling
+    end
+
+    it "can touch out" do
+      subject.topup(Oystercard::MIN_BALANCE)
+      subject.touch_in
+      subject.touch_out
+      expect(subject).not_to be_travelling
+    end
+
   end
 
-  it "can touch out" do
-    subject.touch_in
-    subject.touch_out
-    expect(subject).not_to be_travelling
+  describe "#touch_in" do
+    it "checks balance is above min amount" do
+      expect { subject.touch_in }.to raise_error "balance too low"
+    end
   end
 
-  # it "returns true / false for journey" do
-  #   expect(subject.in_journey?).to eq(false)
-  # end
-  #   it "allows user to touch in" do
-  #     subject.touch_in
-  #     expect(subject.in_journey?).to eq(true)
-  #   end
-  #
-  #   it "allows user to touch out" do
-  #     subject.touch_out
-  #     expect(subject.in_journey?).to eq(false)
-  #   end
-end
+
+
 
 end
